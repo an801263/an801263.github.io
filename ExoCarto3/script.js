85e5a616edcdceeb22d2b1620be1bf8788a5f8ac
AITTAHAR Nawel <nawwela@gmail.com>
2021-01-06 15:17:42 +0100

    exotest

--- ExoCarto3/script.js
+++ ExoCarto3/script.js
@@ -6,15 +6,16 @@ if (window.DeviceMotionEvent) {
 if(window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", process, false);
   } else {
-    // Le navigateur ne supporte pas l'événement deviceorientation
-  }
+    console.log("no supported");  
+}
 function process(event) {
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
-    document.getElementById("log").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
+   
     var alpha = event.alpha;
     var beta = event.beta;
     var gamma = event.gamma;
     document.getElementById("log2").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>";
+ var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+    document.getElementById("log").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
 }
