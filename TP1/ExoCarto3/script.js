1d8b4618fb92bf3989bd887cb80905aa7d37c301
AITTAHAR Nawel <nawwela@gmail.com>
2021-01-13 11:49:24 +0100

    ex

--- TP1/ExoCarto3/script.js
+++ TP1/ExoCarto3/script.js
@@ -3,18 +3,19 @@ if (window.DeviceMotionEvent) {
 } else {
     console.log("no supported");
 }
+
 if(window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", process, false);
   } else {
     console.log("no supported");  
 }
+
 function process(event) {
-   
     var alpha = event.alpha;
     var beta = event.beta;
     var gamma = event.gamma;
     document.getElementById("log2").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>";
- var x = event.accelerationIncludingGravity.x;
+    var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
     document.getElementById("log").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
